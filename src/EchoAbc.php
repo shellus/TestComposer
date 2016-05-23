f3a1188a787936e4e8e2ea1943249b3f74b8291e
shellus <shellus@endaosi.com>
2016-05-23 16:55:26 +0800

    first commit

---
+++ src/EchoAbc.php
@@ -0,0 +1,14 @@
+<?php namespace Shellus\TestComposer;
+
+/**
+ * Created by PhpStorm.
+ * User: shellus
+ * Date: 2016-05-23
+ * Time: 16:51
+ */
+class EchoAbc
+{
+    public function dump(){
+        echo 'abc';
+    }
+}
